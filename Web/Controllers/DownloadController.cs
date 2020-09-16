b7e55fe5d60ea338fc2fd9521af09c83456326ba
Ondřej Václavek <vaclavek@havit.cz>
2020-09-16 10:48:55 +0200

    Initial commit

---
+++ Web/Controllers/DownloadController.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DownloadController : ControllerBase
+    {
+        public async Task<IActionResult> GetFile(string fileName)
+        {
+            var fi = new FileInfo(Path.Combine("Files", fileName));
+            var file = await System.IO.File.ReadAllBytesAsync(fi.FullName);
+            return new FileContentResult(file, "application/octet-stream")
+            {
+                FileDownloadName = fileName
+            };
+        }
+    }
+}
---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Files()
+        {
+            var di = new DirectoryInfo("Files");
+            return View(di.GetFiles().Select(f => f.Name).ToArray());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
