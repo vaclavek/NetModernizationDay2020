77c4980359c1d60f98b44438ef39cc841b6958a7
Ondřej Václavek <vaclavek@havit.cz>
2020-09-16 12:26:54 +0200

    Add Task to File actions

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Web.Models;
 
@@ -13,10 +14,10 @@ namespace Web.Controllers
             return View();
         }
 
-        public IActionResult Files()
+        public Task<ViewResult> Files()
         {
             var di = new DirectoryInfo("Files");
-            return View(di.GetFiles().Select(f => f.Name).ToArray());
+            return Task.FromResult(View(di.GetFiles().Select(f => f.Name).ToArray()));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
